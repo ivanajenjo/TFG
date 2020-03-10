fdf7d8eb49cff4733048e7f675645300cd04751a
Ivan Ajenjo <ivan.ajenjo.vicente@gmail.com>
2020-03-10 16:09:36 +0100

    Reunion commit

--- src/testing.py
+++ src/testing.py
@@ -1,7 +1,16 @@
 import pandas as pd
 
-archivo = 'data\ISBSG - Release May 2017 R1.csv'
+archivo = '..\data\ISBSG - Release May 2017 R1.csv'
+#archivo = '..\data\ISBSG DATA Release 12.csv'
 df = pd.read_csv(archivo, sep = ';', low_memory = False)
-variables = ['ISBSG Project ID', 'Data Quality Rating', 'UFP rating', 'Application Group', 'Development Type', 'Development Platform', 'Language Type', 'Primary Programming Language', 'Count Approach', 'Functional Size', 'Adjusted Function Points', 'Normalised Work Effort Level 1', 'Summary Work Effort', 'Project Elapsed Time', 'Business Area Type', 'Used Methodology', 'Resource Level', 'Max Team Size', 'Average Team Size', 'Input count', 'Output count', 'Enquiry count', 'File count', 'Interface count', 'Agile Method Used']
+variables = ['Data Quality Rating', 'UFP rating', 'Industry Sector','Application Group', 'Development Type', 'Development Platform', 'Language Type', 'Primary Programming Language', 'Count Approach', 'Functional Size', 'Adjusted Function Points', 'Normalised Work Effort Level 1', 'Summary Work Effort', 'Project Elapsed Time', 'Business Area Type', '1st Data Base System', 'Used Methodology', 'Resource Level', 'Max Team Size', 'Average Team Size', 'Input count', 'Output count', 'Enquiry count', 'File count', 'Interface count', 'Agile Method Used']
+df = df.loc[:, variables]
+filtro = ((df['Data Quality Rating'] == 'A') | (df['Data Quality Rating'] == 'B')) & ((df['UFP rating'] == 'A') | (df['UFP rating'] == 'B')) 
+df = df.loc[filtro, :]
+filtro = (df['Normalised Work Effort Level 1'].notnull()) & (df['Normalised Work Effort Level 1'] == df['Summary Work Effort'])
+df = df.loc[filtro, :]
+filtro = df['Count Approach'] == 'IFPUG 4+'
+df = df.loc[filtro, :]
+variables = ['Industry Sector','Application Group', 'Development Type', 'Development Platform', 'Language Type', 'Primary Programming Language', 'Functional Size', 'Adjusted Function Points', 'Normalised Work Effort Level 1', 'Project Elapsed Time', 'Business Area Type', '1st Data Base System', 'Used Methodology', 'Max Team Size', 'Average Team Size', 'Input count', 'Output count', 'Enquiry count', 'File count', 'Interface count', 'Agile Method Used']
 df = df.loc[:, variables]
 df
